c3c91c578dc0274ad4ca016f8e66d323ac18f64f
Patrick Coffey <patrickcoffey48@gmail.com>
2016-02-11 15:12:15 -0800

    Progress on conjuree

--- conjurer.js
+++ conjurer.js
@@ -5,8 +5,21 @@
 
 'use strict'
 
+const Conjuree = require('./lib/conjuree');
+
+// Test implementation.
+let machine = new Conjuree();
+machine.setName('Testing');
+machine.setPort(1337);
+machine.setMethod(() => {
+  return 'hiiiii';
+});
+
+machine.listen();
+
 // Export conjurer and conjuree libraries.
 module.exports = {
   Conjurer: require('./lib/conjurer'),
   Conjuree: require('./lib/conjuree'),
 }
+
