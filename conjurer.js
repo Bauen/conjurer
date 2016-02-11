7c624a02eac219efe440ccedbd85348528378e01
Patrick Coffey <patrickcoffey48@gmail.com>
2016-02-10 18:14:56 -0800

    Empty conjurer and conjuree classes

--- conjurer.js
+++ conjurer.js
@@ -2,3 +2,11 @@
  * @file
  * Main package file for conjurer.
  */
+
+'use strict'
+
+// Export conjurer and conjuree libraries.
+module.exports = {
+  Conjurer: require('./lib/conjurer'),
+  Conjuree: require('./lib/conjuree'),
+}
